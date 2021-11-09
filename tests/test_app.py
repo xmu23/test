de23e59582f6fc5f8601cb9a09f6073bc459c5a6
Xiangxi Mu <xmu23@colby.edu>
2021-11-09 17:56:07 -0500

    create template

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,4 @@ from app import *
 def test_about():
 	
 	# simple way to test webpage (words for basic pages)
-	assert about() == "About us: Naser and the cool kids from CS321"
+	assert about() == "TOM's Website"
