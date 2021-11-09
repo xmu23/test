7adb01fbe43fb2dcd73450ef8c37c09c4c0379f3
Xiangxi Mu <xmu23@colby.edu>
2021-11-09 17:44:10 -0500

    create template

--- src/app.py
+++ src/app.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello, World!"
+    user = {'username' : 'Tom' }
+    return render_template("base.html", title="home", user=user)
 
-@app.route("/about")
-def about():
-    return "Xiangxi Mu"
 
 if __name__ == "__main__":
     app.run(debug = True)
