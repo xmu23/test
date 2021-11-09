2bbb8c77ae23cf14c04d49f80e57182cad2a76ff
Xiangxi Mu <xmu23@colby.edu>
2021-11-09 17:55:16 -0500

    create template

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,7 @@
-from app import index
+from app import *
 
 
 def test_about():
 	
 	# simple way to test webpage (words for basic pages)
 	assert about() == "About us: Naser and the cool kids from CS321"
-
-	# more intreesting way to test the the page works
-	client = app.test_client()
-	response = client.get("/about")
-	assert response.status_code == 200  # success
