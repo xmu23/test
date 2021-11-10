5fe13b183cc85db88e69f0c02894705527b3e02e
Xiangxi Mu <xmu23@colby.edu>
2021-11-09 19:09:34 -0500

    finish the tutorial

--- src/app.py
+++ src/app.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     user = {'username' : 'Tom' }
-    return render_template("base.html", title="home", user=user)
+    return render_template("base.html", title="home", user=user, visitors=visitors)
 
 @app.route("/about")
 def about():
     return "TOM's Website"
+
+visitors = []
+@app.route("/add", methods=["POST"])
+def add():
+    visitor = request.form.get("visitor")
+    visitors.append(visitor)
+    print(visitors[-1])
+    return redirect(url_for("index"))
+
+@app.route("/remove/<string:name>")
+def remove(name):
+    visitors.remove(name)
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug = True)
